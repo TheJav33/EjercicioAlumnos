0a0c160e8839e735285fcecdd57e666ad48ed5b5
Martina <ma.quezadan@duocuc.cl>
2026-06-03 19:51:23 -0400

    modificacion

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,10 @@
 print("Hola mundo desde Github")
 print("Nuevo cambio")  
+while True:
+    numero = int(input("Ingrese un numero entero: "))
+    if numero >= 0:
+        print("Correcto")
+        break
+    else:
+        print("Intente nuevamente")
+        int(input("Ingrese un numero entero: "))
